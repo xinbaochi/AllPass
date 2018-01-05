f263399a8e21f863ac6032cc7c971b962f20a8bb
nfr472xb <nfr472xb@gmail.com>
2018-01-05 11:04:55 +0800

    V2

--- part/footer.php
+++ part/footer.php
@@ -1,9 +1,9 @@
 
 <!-- Compiled and minified JavaScript -->
 
-<link rel=stylesheet type="text/css" href="\source\css\materialize.min.css">
-<link rel=stylesheet type="text/css" href="\source\css\style.css">
-<script src="/source/js/jquery-2.1.2.min.js"></script>
-<script src="/source/js/materialize.min.js"></script>
-<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-<script src="/source/js/init.js"></script>
+<link rel=stylesheet type="text/css" href=".\source\css\materialize.min.css">
+<link rel=stylesheet type="text/css" href=".\source\css\style.css">
+<script src="./source/js/jquery-2.1.2.min.js"></script>
+<script src="./source/js/materialize.min.js"></script>
+<link rel=stylesheet type="text/css" href=".\source\css\icon.css">
+<script src="./source/js/init.js"></script>
