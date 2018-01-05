40b37bbae50605d0e362e5f4d16c4d4d668d0ad7
nfr472xb <nfr472xb@gmail.com>
2018-01-05 12:30:06 +0800

    add

--- part/nav.php
+++ part/nav.php
@@ -1,10 +1,25 @@
 <nav class="cyan lighten-1" role="navigation">
+
      <div class="nav-wrapper container"><a id="logo-container" href="#" class="brand-logo">All Pass</a>
+       
+       
+       
+   
+
        <ul class="right hide-on-med-and-down">
+       
          <li><a href="#">課程資訊</a></li>
          <li><a href="#">登入</a></li>
        </ul>
- 
+       <div class="right nav-wrapper">
+      <form>
+        <div class="nav-input input-field">
+          <input id="search" type="search" required>
+          <label class="label-icon" for="search"><i class="material-icons">search</i></label>
+          <i class="material-icons">close</i>
+        </div>
+      </form>
+    </div>
        <ul id="nav-mobile" class="side-nav">
        <li><a href="#">課程資訊</a></li>
        <li><a href="#">登入</a></li>
