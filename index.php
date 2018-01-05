239eb461040ec6108712d384e4755f33d717d937
nfr472xb <nfr472xb@gmail.com>
2018-01-05 13:11:39 +0800

    首頁

--- index.php
+++ index.php
@@ -3,4 +3,4 @@
       <?php include("part/header.php"); ?>
 
       <?php include("content/index-content.php"); ?>
-      <?php include("part/footer.php"); ?>
+      <?php include("part/footer.php"); ?> 
