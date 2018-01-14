628957a6730c9177adbe96c17c3f815356578c64
nfr472xb <nfr472xb@gmail.com>
2018-01-14 14:30:34 +0800

    +

--- content/class-list-content.php
+++ content/class-list-content.php
@@ -12,11 +12,11 @@
 <li class="collection-header"><h4>課程列表</h4></li>
 <li class="collection-item"><div>
 	<a href=".\class-info.php">科普讀物導讀</a>
-	&nbsp&nbsp<a href=".\class-info.php">胡忠信</a>
+	&nbsp&nbsp<a href=".\teacher-info.php">胡忠信</a>
 	<a href="#!" class="secondary-content"><i class="material-icons">favorite_border</i></a></div></li>
 <li class="collection-item"><div>
 	<a href=".\class-info.php">科普讀物導讀</a>
-	&nbsp&nbsp<a href=".\class-info.php">洪鼎惟</a>
+	&nbsp&nbsp<a href=".\teacher-info.php">洪鼎惟</a>
 	<a href="#!" class="secondary-content"><i class="material-icons">favorite</i></a></div></li>
 </ul>
 
